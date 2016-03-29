83ba1aed4872ea844ddaf0c0b9586b432fae23df
vikram K <vikramkb@thoughtworks.com>
2016-03-29 22:50:57 +0530

    Given from and to, search displays flight number, from and to locations

---
+++ src/main/java/com/twair/FlightSearch.java
@@ -0,0 +1,30 @@
+package com.twair;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class FlightSearch {
+
+    private List<Flight> flightList;
+
+    public FlightSearch(List<Flight> flightList) {
+        this.flightList = flightList;
+    }
+
+    public List<Flight> getFlightList() {
+        return flightList;
+    }
+
+    public FlightSearch byLocation(String source, String destination) {
+        if(source == null || source.isEmpty() || destination == null || destination.isEmpty()) {
+            throw new IllegalArgumentException("source cannot be null");
+        }
+        List<Flight> matchingFlights = new ArrayList<Flight>();
+        for (Flight flight : flightList) {
+            if (flight.getSource().equals(source) && flight.getDestination().equals(destination)) {
+                matchingFlights.add(flight);
+            }
+        }
+        return new FlightSearch(matchingFlights);
+    }
+}
---
+++ src/main/java/com/twair/SearchForm.java
@@ -0,0 +1,31 @@
+package com.twair;
+
+public class SearchForm {
+    private String from;
+    private String to;
+    private String number;
+
+    public String getFrom() {
+        return from;
+    }
+
+    public void setFrom(String from) {
+        this.from = from;
+    }
+
+    public String getTo() {
+        return to;
+    }
+
+    public void setTo(String to) {
+        this.to = to;
+    }
+
+    public String getNumber() {
+        return number;
+    }
+
+    public void setNumber(String number) {
+        this.number = number;
+    }
+}
